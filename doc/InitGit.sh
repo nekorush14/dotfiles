de2fdb0274dcd68623c3bb829de30bfb4525a3fa
Mitsuhiro Komuro <applemitkom@gmail.com>
2018-02-11 21:25:49 +0900

    Fix some files.

--- doc/InitGit.sh
+++ doc/InitGit.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-#if [ $# -ne 1 ]; then
-#  echo "Illegal arguments format."
-#  echo "Shell script stoped."
-#  exit -1
-#fi
-
 PATH=$1
-
-#if [ ${PATH} -eq "~/shellsrc" ] then
-#	echo "Can't running script"
-#	exit -1
-#fi
 
 cd ${PATH}
 
@@ -24,14 +13,9 @@ cd ${PATH}
 # Change the git editor to vim.
 /usr/bin/git config --global core.editor "vim"
 
-# Registration my name.
-/usr/bin/git config --global user.name "Mitsuhiro Komuro"
-
-# Registration my e-mail address.
-/usr/bin/git config --global user.email applemitkom@gmail.com
-
 echo "Git initializeing has been compleated."
 echo ""
+echo You need to setup the Git global user name and email address.
 echo If you need to push remote repository,
 echo you should type follow command.\n
 echo 1. git remote add origin git@yourRepository
