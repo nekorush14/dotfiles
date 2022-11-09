4c7211cc6a5af0a99fcab68f53b82d61ccba92f4
Mitsuhiro Komuro <mitsuhiro.komuro@gmail.com>
2022-11-10 00:41:27 +0900

    Add ps1 files

---
+++ dev/posh/user_profile.ps1
@@ -0,0 +1,7 @@
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias which gcm
